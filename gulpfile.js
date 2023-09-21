2d4e02d42bf8d5a0eb3c1945d5a893e0788efcb0
ercrrc <134560333+ercrrc@users.noreply.github.com>
2023-09-21 12:00:35 -0300

    correcao2

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,14 @@ const sass = require('gulp-sass')(require('sass'));
 const obfuscate = require('gulp-obfuscate');
 const imagemin =require('gulp-imagemin');
 
+function compilaSass() {
+    return gulp.src('./source/images/main.scss')
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(gulp.dest(source/images));
+}
+
 function comprimeImagens() {
     return gulp.src('./source/images/*')
         .pipe(imagemin())
@@ -13,6 +21,7 @@ function comprimeJavaScript() {
     return gulp.src('./source/*js')
         .pipe(uglify())
         .pipe(obfuscate())
+        .pipe(gulp.dest('./source/images'));
 }
 
 
